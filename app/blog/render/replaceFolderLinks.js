83d7cd72b05f35b88bc8401c96d81ad518402d6e
David Merfield <dmerfield@gmail.com>
2025-03-26 15:35:13 +0100

    Updte

--- app/blog/render/replaceFolderLinks.js
+++ app/blog/render/replaceFolderLinks.js
@@ -2,43 +2,26 @@ const parse5 = require("parse5");
 const config = require("config");
 const fs = require("fs-extra");
 const hash = require("helper/hash");
-const { at } = require("lodash");
 
 class Cache {
-  constructor(maxBytes = 1024 * 1024) {
+  constructor() {
     this.cache = new Map();
-    this.maxBytes = maxBytes;
-    this.currentSize = 0;
+    this.maxEntries = 10000; // roughly 1MB of data
   }
 
   set(key, value) {
-    const valueSize = Buffer.from(value).length;
-    if (valueSize > this.maxBytes) return;
-
-    if (this.currentSize + valueSize > this.maxBytes) {
-      const entriesToDelete = [];
-      let bytesToFree = this.currentSize + valueSize - this.maxBytes;
-
-      for (const [key, val] of this.cache) {
-        bytesToFree -= Buffer.from(val).length;
-        entriesToDelete.push(key);
-        if (bytesToFree <= 0) break;
-      }
-
-      for (const key of entriesToDelete) {
-        const val = this.cache.get(key);
-        this.currentSize -= Buffer.from(val).length;
-        this.cache.delete(key);
-      }
+    if (this.cache.size >= this.maxEntries) {
+      // Delete oldest entry (first item in map)
+      const firstKey = this.cache.keys().next().value;
+      this.cache.delete(firstKey);
     }
-
     this.cache.set(key, value);
-    this.currentSize += valueSize;
   }
 
   get(key) {
     const value = this.cache.get(key);
     if (value) {
+      // Move to end by deleting and re-adding
       this.cache.delete(key);
       this.cache.set(key, value);
     }
@@ -51,17 +34,40 @@ const pathCache = new Cache();
 const htmlExtRegex = /\.html$/;
 const fileExtRegex = /\/[^/]*\.[^/]*$/;
 
+async function getVersion(blogID, cacheID, value) {
+  const cacheKey = hash(`${blogID}:${cacheID}:${value}`);
+  const cachedVersion = pathCache.get(cacheKey);
+
+  if (cachedVersion) {
+    return cachedVersion;
+  }
+
+  try {
+    const filePath = `${config.blog_folder_dir}/${blogID}${value}`;
+    const stat = await fs.stat(filePath);
+    const version = hash(`${stat.mtime}${stat.size}`).slice(0, 8);
+    pathCache.set(cacheKey, version);
+    return version;
+  } catch (err) {
+    console.log(cacheKey, `File not found: ${value}`, err);
+    pathCache.set(cacheKey, "ENOENT");
+    return "ENOENT";
+  }
+}
+
 module.exports = async function replaceFolderLinks(cacheID, blogID, html) {
   try {
-    const fragment = parse5.parseFragment(html);
-    
+    // Changed from parseFragment to parse for full HTML document
+    const document = parse5.parse(html);
+
     const elements = [];
     const promises = [];
-    
-    const stack = [fragment];
+
+    // Start with document.childNodes instead of the document itself
+    const stack = [...document.childNodes];
 
     let changes = 0;
-    
+
     while (stack.length > 0) {
       const node = stack.pop();
 
@@ -93,35 +99,19 @@ module.exports = async function replaceFolderLinks(cacheID, blogID, html) {
           !htmlExtRegex.test(attr.value) &&
           fileExtRegex.test(attr.value)
         ) {
-          const cacheKey = `${cacheID}:${attr.value}`;
-          const cachedResult = pathCache.get(cacheKey);
-
-          if (cachedResult) {
-            // we have cached a missing file
-            if (cachedResult === attr.value) {
-              continue;
-            }
-            attr.value = cachedResult;
-            changes++;
-            continue;
-          }
-
           promises.push(
             (async () => {
-              try {
-                const stat = await fs.stat(
-                  config.blog_folder_dir + "/" + blogID + attr.value
-                );
-                const identifier = stat.mtime.toString() + stat.size.toString();
-                const version = hash(identifier).slice(0, 8);
-                const result = `${config.cdn.origin}/folder/v-${version}/${blogID}${attr.value}`;
-                pathCache.set(cacheKey, result);
-                attr.value = result;
-                changes++;
-              } catch (err) {
-                console.warn(`File not found: ${attr.value}`, err);
-                pathCache.set(cacheKey, attr.value);
-              } 
+              const version = await getVersion(blogID, cacheID, attr.value);
+
+              if (version === "ENOENT") {
+                console.log(`File not found: ${attr.value}`);
+                return;
+              }
+
+              const result = `${config.cdn.origin}/folder/v-${version}/${blogID}${attr.value}`;
+              console.log(`Replacing ${attr.value} with ${result}`);
+              attr.value = result;
+              changes++;
             })()
           );
         }
@@ -129,7 +119,8 @@ module.exports = async function replaceFolderLinks(cacheID, blogID, html) {
     }
 
     await Promise.all(promises);
-    return promises.length > 0 ? parse5.serialize(fragment) : html;
+    // Use serialize on the full document
+    return changes ? parse5.serialize(document) : html;
   } catch (err) {
     console.warn("Parse5 parsing failed:", err);
     return html;
