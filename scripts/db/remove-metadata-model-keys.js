cca0fcd8fa83af801aa2ff6c36de2ef8de878fdc
David Merfield <dmerfield@gmail.com>
2025-03-27 13:40:14 +0100

    Adds scripts

---
+++ scripts/db/remove-metadata-model-keys.js
@@ -0,0 +1,8 @@
+// return "blog:" + blogID + ":folder:everything";
+// },
+
+// path: function (blogID, path) {
+//   return "blog:" + blogID + ":folder:" + pathNormalizer(path);
+
+const keys = require('../util/redisKeys');
+
