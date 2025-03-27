362b155bc749579aba6b2859e6ed1a39268f7383
David Merfield <dmerfield@gmail.com>
2025-03-27 09:44:22 +0100

    Removes metadata model

--- app/sync/update/drop.js
+++ app/sync/update/drop.js
@@ -1,6 +1,5 @@
 var async = require("async");
 var Entry = require("models/entry");
-var Metadata = require("models/metadata");
 var Ignored = require("models/ignoredFiles");
 var Preview = require("./preview");
 var isDraft = require("./drafts").isDraft;
@@ -13,7 +12,6 @@ module.exports = function (blogID, path, options, callback) {
     // ORDER IS IMPORTANT
     // Rebuild must happen after we remove the file from disk
     var queue = [
-      Metadata.drop.bind(this, blogID, path),
       Ignored.drop.bind(this, blogID, path),
       Entry.drop.bind(this, blogID, path),
       rebuildDependents.bind(this, blogID, path),
