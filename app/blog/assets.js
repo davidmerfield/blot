6a30b47b3c43f60c61dd5e43e917587c1bcef4e6
David Merfield <dmerfield@gmail.com>
2025-03-29 15:39:46 +0100

    Update assets

--- app/blog/assets.js
+++ app/blog/assets.js
@@ -1,18 +1,25 @@
 const config = require("config");
 const express = require("express");
 const mime = require("mime-types");
-const debug = require("debug")("blot:blog:assets");
 const { join, basename, dirname } = require("path");
 const { promisify } = require("util");
 const fs = require("fs-extra");
-
 const caseSensitivePath = promisify(require("helper/caseSensitivePath"));
 
 // Constants
-const GLOBAL_STATIC_FILES = config.blot_directory + "/app/blog/static";
 const LARGEST_POSSIBLE_MAXAGE = 86400000;
-const BLOCKED_PATTERNS = ["..", ".php", "/.git", "\0"];
-const STATIC_DIRS = ["/fonts", "/icons", "/katex", "/plugins"];
+
+const GLOBAL_STATIC_FILES = config.blot_directory + "/app/blog/static";
+const GLOBAL_STATIC_SUBDIRECTORIES = ["/fonts", "/icons", "/katex", "/plugins"];
+
+// We always return 404 for requests which contain these patterns
+const BLOCKED_PATTERNS = [
+  "..", // parent directory traversal
+  ".php", // blot does not support PHP
+  "/.git", // avoid exposing git directories
+  "\0", // null byte
+];
+
 const BLOG_STATIC_PATHS = [
   "/_assets",
   "/_avatars",
@@ -21,10 +28,128 @@ const BLOG_STATIC_PATHS = [
   "/_thumbnails",
 ];
 
-// Utility functions
-function createStaticMiddleware(path) {
-  return express.static(path, { maxAge: "1y" });
-}
+// Router setup
+const assets = express.Router();
+
+// Security middleware
+assets.use((req, res, next) => {
+  if (
+    BLOCKED_PATTERNS.some(
+      (pattern) =>
+        req.path.includes(pattern) ||
+        decodeURIComponent(req.path).includes(pattern)
+    )
+  ) {
+    return next(new Error("Not Found"));
+  }
+  next();
+});
+
+// Global static files
+GLOBAL_STATIC_SUBDIRECTORIES.forEach((dir) => {
+  assets.use(dir, express.static(GLOBAL_STATIC_FILES + dir, { maxAge: "1y" }));
+});
+
+assets.get("/html2canvas.min.js", (req, res) => {
+  sendFile(GLOBAL_STATIC_FILES + "/html2canvas.min.js", {
+    req,
+    res,
+    maxAge: LARGEST_POSSIBLE_MAXAGE,
+    immutable: true,
+  });
+});
+
+assets.get("/layout.css", (req, res) => {
+  sendFile(GLOBAL_STATIC_FILES + "/layout.css", {
+    req,
+    res,
+    maxAge: LARGEST_POSSIBLE_MAXAGE,
+    immutable: true,
+  });
+});
+
+// Blog-specific static assets
+assets.use(BLOG_STATIC_PATHS, async (req, res, next) => {
+  try {
+    const filePath =
+      config.blog_folder_dir + "/" + req.blog.id + decodeURIComponent(req.path);
+    await sendFile(filePath, {
+      req,
+      res,
+      maxAge: LARGEST_POSSIBLE_MAXAGE,
+      immutable: true,
+    });
+  } catch (err) {
+    next();
+  }
+});
+
+// Try to serve files from the blog folder
+assets.use(async (req, res, next) => {
+  const blogFolder = config.blog_folder_dir + "/" + req.blog.id;
+  const decodedPath = decodeURIComponent(req.path);
+
+  try {
+    await sendFile(join(blogFolder, decodedPath), { req, res });
+    return;
+  } catch (e) {}
+
+  try {
+    await sendFile(join(blogFolder, decodedPath.toLowerCase()), { req, res });
+    return;
+  } catch (e) {}
+
+  try {
+    const pathWithCorrectCase = await caseSensitivePath(
+      blogFolder,
+      decodedPath
+    );
+
+    const stat = await fs.stat(pathWithCorrectCase);
+
+    if (!stat.isFile()) throw new Error("Not a file");
+
+    await sendFile(pathWithCorrectCase, { req, res });
+    return;
+  } catch (e) {}
+
+  try {
+    await sendFile(
+      join(blogFolder, withoutTrailingSlash(decodedPath) + "/index.html"),
+      { req, res }
+    );
+    return;
+  } catch (e) {}
+
+  try {
+    await sendFile(
+      join(blogFolder, withoutTrailingSlash(decodedPath) + ".html"),
+      { req, res }
+    );
+    return;
+  } catch (e) {}
+
+  try {
+    await sendFile(
+      join(blogFolder, addLeadingUnderscore(decodedPath) + ".html"),
+      { req, res }
+    );
+    return;
+  } catch (e) {}
+
+  // If we get here, none of the candidates worked
+  if (!res.headersSent) {
+    next();
+  }
+});
+
+// Error handling
+assets.use((err, req, res, next) => {
+  if (err && err.message === "Not Found") {
+    return next();
+  }
+  next(err);
+});
 
 function withoutTrailingSlash(path) {
   return path && path.slice(-1) === "/" ? path.slice(0, -1) : path;
@@ -35,7 +160,7 @@ function addLeadingUnderscore(path) {
   return join(dirname(path), "_" + basename(path));
 }
 
-function sendFile(res, path, { req, maxAge = 0, immutable = false } = {}) {
+function sendFile(path, { req, res, maxAge = 0, immutable = false } = {}) {
   const isDirectory = path.indexOf(".") === -1;
   const defaultMime = isDirectory ? "text/html" : "application/octet-stream";
   let contentType = mime.contentType(mime.lookup(path) || defaultMime);
@@ -68,128 +193,5 @@ function sendFile(res, path, { req, maxAge = 0, immutable = false } = {}) {
     });
   });
 }
-
-// Router setup
-const assets = express.Router();
-
-// Security middleware
-assets.use((req, res, next) => {
-  if (
-    BLOCKED_PATTERNS.some(
-      (pattern) =>
-        req.path.includes(pattern) ||
-        decodeURIComponent(req.path).includes(pattern)
-    )
-  ) {
-    return next(new Error("Not Found"));
-  }
-  next();
-});
-
-// Global static files
-STATIC_DIRS.forEach((dir) => {
-  assets.use(dir, createStaticMiddleware(GLOBAL_STATIC_FILES + dir));
-});
-
-assets.get("/html2canvas.min.js", (req, res) => {
-  sendFile(res, GLOBAL_STATIC_FILES + "/html2canvas.min.js", {
-    req,
-    maxAge: LARGEST_POSSIBLE_MAXAGE,
-    immutable: true,
-  });
-});
-
-assets.get("/layout.css", (req, res) => {
-  sendFile(res, GLOBAL_STATIC_FILES + "/layout.css", {
-    req,
-    maxAge: LARGEST_POSSIBLE_MAXAGE,
-    immutable: true,
-  });
-});
-
-// Blog-specific static assets
-assets.use(BLOG_STATIC_PATHS, async (req, res, next) => {
-  try {
-    const filePath =
-      config.blog_folder_dir + "/" + req.blog.id + decodeURIComponent(req.path);
-    await sendFile(res, filePath, {
-      req,
-      maxAge: LARGEST_POSSIBLE_MAXAGE,
-      immutable: true,
-    });
-  } catch (err) {
-    next();
-  }
-});
-
-// Try to serve files from the blog folder
-assets.use(async (req, res, next) => {
-  const blogFolder = config.blog_folder_dir + "/" + req.blog.id;
-  const decodedPath = decodeURIComponent(req.path);
-
-  try {
-    await sendFile(res, join(blogFolder, decodedPath), { req });
-    return;
-  } catch (e) {}
-
-  try {
-    await sendFile(res, join(blogFolder, decodedPath.toLowerCase()), { req });
-    return;
-  } catch (e) {}
-
-  try {
-    const pathWithCorrectCase = await caseSensitivePath(
-      blogFolder,
-      decodedPath
-    );
-
-    const stat = await fs.stat(pathWithCorrectCase);
-
-    if (!stat.isFile()) throw new Error("Not a file");
-
-    await sendFile(res, pathWithCorrectCase, { req });
-    return;
-  } catch (e) {}
-
-  try {
-    await sendFile(
-      res,
-      join(blogFolder, withoutTrailingSlash(decodedPath) + "/index.html"),
-      { req }
-    );
-    return;
-  } catch (e) {}
-
-  try {
-    await sendFile(
-      res,
-      join(blogFolder, withoutTrailingSlash(decodedPath) + ".html"),
-      { req }
-    );
-    return;
-  } catch (e) {}
-
-  try {
-    await sendFile(
-      res,
-      join(blogFolder, addLeadingUnderscore(decodedPath) + ".html"),
-      { req }
-    );
-    return;
-  } catch (e) {}
-
-  // If we get here, none of the candidates worked
-  if (!res.headersSent) {
-    next();
-  }
-});
-
-// Error handling
-assets.use((err, req, res, next) => {
-  if (err && err.message === "Not Found") {
-    return next();
-  }
-  next(err);
-});
 
 module.exports = assets;
