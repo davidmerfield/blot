ff193d68a39493c1df96a9daf906e1d18ff70c9b
David Merfield <dmerfield@gmail.com>
2025-10-13 10:56:59 +0200

    Simplify

--- app/clients/local/routes.js
+++ app/clients/local/routes.js
@@ -19,23 +19,10 @@ Dashboard.get("/", function (req, res) {
   setup(req.blog.id, function (err) {
     if (err) console.log("Error setting up", err);
   });
-  res.locals.localClientFolderPath = `data/blogs/${req.blog.id}`;
-  const basePath = res.locals.base || req.baseUrl || "";
-  res.locals.localClientOpenFolderAction = `${basePath}/open`;
-  const openStatus = req.query.open;
-  if (openStatus === "success") {
-    res.locals.localClientOpenFolderStatus = "Opening folder…";
-    res.locals.localClientOpenFolderIsError = false;
-  } else if (openStatus === "error") {
-    res.locals.localClientOpenFolderStatus =
-      "Could not open the folder. Is the open-folder server running?";
-    res.locals.localClientOpenFolderIsError = true;
-  }
   res.render(__dirname + "/views/index.html");
 });
 
-Dashboard.get("/open", async function (req, res) {
-  const basePath = res.locals.base || req.baseUrl || "";
+Dashboard.get("/open", async function (req, res, next) {
   const folderPath = `data/blogs/${req.blog.id}`;
   try {
     const response = await fetch(`${DEFAULT_OPEN_FOLDER_ORIGIN}/open-folder`, {
@@ -50,10 +37,9 @@ Dashboard.get("/open", async function (req, res) {
       throw new Error("Request failed");
     }
 
-    res.redirect(`${basePath}?open=success`);
+    res.redirect(res.locals.base);
   } catch (error) {
-    console.error("Failed to open folder via helper server", error);
-    res.redirect(`${basePath}?open=error`);
+    next(new Error("Could not open folder on your computer"));
   }
 });
 
