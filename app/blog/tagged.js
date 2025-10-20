0176193a7b9950fdac3011222f38eb3b2be7ac11
David Merfield <dmerfield@gmail.com>
2025-10-20 20:54:03 +0200

    Simplfiy tagged page

--- app/blog/tagged.js
+++ app/blog/tagged.js
@@ -7,29 +7,11 @@ module.exports = function (server) {
       retrieveTagged(request, function (err, result) {
         if (err) return next(err);
 
-        const slug = request.params.tag;
-        const totalEntries =
-          result && result.total !== undefined
-            ? result.total
-            : (result && result.entries ? result.entries.length : 0);
-
-        response.locals.tagged = result || {};
-        response.locals.tag = (result && result.tag) || slug;
-        response.locals.slug = slug;
+        response.locals.slug = request.params.tag;
+        response.locals.tag = (result && result.tag) || request.params.tag;
         response.locals.entries = (result && result.entries) || [];
-        response.locals.total = totalEntries;
-        response.locals.pagination = result ? result.pagination : undefined;
-        response.locals.is = response.locals.tagged
-          ? response.locals.tagged.tagged
-          : undefined;
-
-        if (response.locals.tagged) {
-          response.locals.tagged.total = totalEntries;
-          response.locals.tagged.entries = response.locals.entries;
-          response.locals.tagged.pagination = response.locals.pagination;
-          response.locals.tagged.tag = response.locals.tag;
-          response.locals.tagged.slug = slug;
-        }
+        response.locals.total = (result && result.total) || 0;
+        response.locals.pagination = (result && result.pagination) || {};
 
         response.renderView("tagged.html", next);
       });
