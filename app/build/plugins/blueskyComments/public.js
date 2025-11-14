b6f23f3dc943ad079d64bec9671815b69a875715
David Merfield <dmerfield@gmail.com>
2025-11-14 12:55:45 +0100

    Revise

--- app/build/plugins/blueskyComments/public.js
+++ app/build/plugins/blueskyComments/public.js
@@ -22,21 +22,15 @@ const renderActions = (post) => {
   return `
         <div class="post-actions">
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="var(--highlight)" viewBox="0 0 24 24" stroke-width="1.5" stroke="var(--highlight2)" width="16" height="16">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="m8 14.25.345.666a.75.75 0 0 1-.69 0l-.008-.004-.018-.01a7.152 7.152 0 0 1-.31-.17 22.055 22.055 0 0 1-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.066 22.066 0 0 1-3.744 2.584l-.018.01-.006.003h-.002ZM4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.58 20.58 0 0 0 8 13.393a20.58 20.58 0 0 0 3.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.749.749 0 0 1-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5Z"></path></svg>
                 <span>${post.likeCount}</span>
             </div>
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="var(--highlight2)" width="16" height="16">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M1.705 8.005a.75.75 0 0 1 .834.656 5.5 5.5 0 0 0 9.592 2.97l-1.204-1.204a.25.25 0 0 1 .177-.427h3.646a.25.25 0 0 1 .25.25v3.646a.25.25 0 0 1-.427.177l-1.38-1.38A7.002 7.002 0 0 1 1.05 8.84a.75.75 0 0 1 .656-.834ZM8 2.5a5.487 5.487 0 0 0-4.131 1.869l1.204 1.204A.25.25 0 0 1 4.896 6H1.25A.25.25 0 0 1 1 5.75V2.104a.25.25 0 0 1 .427-.177l1.38 1.38A7.002 7.002 0 0 1 14.95 7.16a.75.75 0 0 1-1.49.178A5.5 5.5 0 0 0 8 2.5Z"></path></svg>
                 <span>${post.repostCount}</span>
             </div>
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="var(--highlight)" viewBox="0 0 24 24" stroke-width="1.5" stroke="var(--highlight2)" width="16" height="16">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M6.78 1.97a.75.75 0 0 1 0 1.06L3.81 6h6.44A4.75 4.75 0 0 1 15 10.75v2.5a.75.75 0 0 1-1.5 0v-2.5a3.25 3.25 0 0 0-3.25-3.25H3.81l2.97 2.97a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L1.47 7.28a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"></path></svg>
                 <span>${post.replyCount}</span>
             </div>
         </div>
@@ -44,6 +38,8 @@ const renderActions = (post) => {
 };
 
 const renderComment = (post) => {
+    console.log(post);
+
   const author = post.author;
   const postUrl = `https://bsky.app/profile/${author.did}/post/${post.uri
     .split("/")
@@ -51,12 +47,16 @@ const renderComment = (post) => {
 
   return `
         <div class="comment-container">
-            <img src="${author.avatar}" alt="avatar">
             <a href="${postUrl}" target="_blank">
-                <span class="author">@${author.handle}</span>
+                <img src="${author.avatar}" alt="avatar">
+            </a>
+            <div>
+                <a href="${postUrl}" target="_blank">
+                    <strong class="author">${author.displayName}</strong>
+                </a>
                 <p>${post.record?.text}</p>
                 ${renderActions(post)}
-            </a>
+            </div>
         </div>
     `;
 };
@@ -129,6 +129,8 @@ const init = () => {
   // Priority 1: Use Bluesky metadata if set
   if (container.dataset.uri) {
     const uri = convertToAtUri(container.dataset.uri);
+    // Add initial content for metadata case
+    container.innerHTML += `<p><a href="${container.dataset.uri}" target="_blank">Reply on Bluesky</a></p>`;
     loadThread(uri, container, container.dataset.uri);
   }
   // Priority 2: Auto-discover using author search
@@ -136,6 +138,9 @@ const init = () => {
     container.dataset.autoDiscover === "true" &&
     container.dataset.author
   ) {
+    // Add loading message
+    container.innerHTML += `<p>Loading comments...</p>`;
+    
     const authorProfileUrl = container.dataset.author;
     const author = extractHandleFromProfileUrl(authorProfileUrl);
     const fetchPost = async () => {
@@ -155,11 +160,11 @@ const init = () => {
           // Update container with comments header
           container.innerHTML = `
                     <h2>Comments</h2>
-                    <p>Post a comment <a href="https://bsky.app/profile/${
+                    <p><a href="https://bsky.app/profile/${
                       post.author.did
                     }/post/${uri
             .split("/")
-            .pop()}" target="_blank">on Bluesky</a>!</p>
+            .pop()}" target="_blank">Reply on Bluesky</a></p>
                 `;
 
           // Add main post actions element
@@ -193,7 +198,9 @@ const init = () => {
     fetchPost();
   }
   // Priority 3: Show error message if no author configured
-  // Error message is already in the HTML from entry.html, no action needed
+  else if (container.dataset.error === "no-author") {
+    container.innerHTML += `<p>Bluesky comments are not configured. Please set a <code>Bluesky</code> metadata field on this entry, or configure your Bluesky author handle in the plugin settings.</p>`;
+  }
 };
 
 init();
