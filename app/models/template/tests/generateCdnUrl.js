48e560d99de8e648460ac6d0af55b0e07fefdb80
David Merfield <dmerfield@gmail.com>
2025-11-15 14:34:59 +0100

    Updates tests

--- app/models/template/tests/generateCdnUrl.js
+++ app/models/template/tests/generateCdnUrl.js
@@ -5,25 +5,30 @@ describe("template", function () {
   const generateCdnUrl = require("../util/generateCdnUrl");
 
   it("generates a CDN URL for a simple view", function () {
-    const url = generateCdnUrl("style.css", "abcdef0123456789");
+    const hash = "abcdef0123456789abcdef0123456789";
+    const url = generateCdnUrl("style.css", hash);
 
     expect(url).toBe(
-      `${config.cdn.origin}/template/style.abcdef0123456789.css`
+      `${config.cdn.origin}/template/ab/cd/${hash}.css`
     );
   });
 
-  it("URI-encodes nested view paths", function () {
-    const url = generateCdnUrl("partials/Header Images/main.scss", "1234abcd");
+  it("generates a CDN URL with nested view paths (view name doesn't affect URL)", function () {
+    const hash = "1234abcd5678ef901234abcd5678ef90";
+    const url = generateCdnUrl("partials/Header Images/main.scss", hash);
 
+    // New format: /template/{hash[0:2]}/{hash[2:4]}/{hash}{ext}
+    // View name is not included in URL, only hash and extension
     expect(url).toBe(
-      `${config.cdn.origin}/template/partials/Header%20Images/main.1234abcd.scss`
+      `${config.cdn.origin}/template/12/34/${hash}.scss`
     );
   });
 
   it("handles view names without an extension", function () {
-    const url = generateCdnUrl("robots", "feedface");
+    const hash = "feedface1234567890feedface12345678";
+    const url = generateCdnUrl("robots", hash);
 
-    expect(url).toBe(`${config.cdn.origin}/template/robots.feedface`);
+    expect(url).toBe(`${config.cdn.origin}/template/fe/ed/${hash}`);
   });
 
   it("throws for invalid arguments", function () {
@@ -32,7 +37,11 @@ describe("template", function () {
     );
 
     expect(() => generateCdnUrl("style.css", "")).toThrowError(
-      /hash must be a non-empty string/
+      /hash must be a non-empty string with at least 4 characters/
+    );
+
+    expect(() => generateCdnUrl("style.css", "abc")).toThrowError(
+      /hash must be a non-empty string with at least 4 characters/
     );
   });
 });
