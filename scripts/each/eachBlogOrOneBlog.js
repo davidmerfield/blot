6e25c9bb4ab3f7cd11f6408b97a865184b5fcf35
David Merfield <dmerfield@gmail.com>
2025-11-17 13:44:58 +0100

    Updates

--- scripts/each/eachBlogOrOneBlog.js
+++ scripts/each/eachBlogOrOneBlog.js
@@ -1,5 +1,7 @@
 const eachBlog = require("./blog");
 const getBlog = require("../get/blog");
+const Blog = require("models/blog");
+const getConfirmation = require("../util/getConfirmation");
 
 /**
  * Processes either a single blog (if identifier provided) or all blogs (if no identifier).
@@ -24,23 +26,51 @@ module.exports = function eachBlogOrOneBlog(processBlog) {
       });
     });
   } else {
-    // Process all blogs
+    // Process all blogs - first get count and ask for confirmation
     return new Promise((resolve, reject) => {
-      eachBlog(
-        async (_user, blog, next) => {
-          try {
-            await processBlog(blog);
-            next();
-          } catch (err) {
-            console.error(`Error processing blog ${blog.id}:`, err.message);
-            next();
-          }
-        },
-        (err) => {
-          if (err) return reject(err);
-          resolve();
+      Blog.getAllIDs(function (err, blogIDs) {
+        if (err || !blogIDs) {
+          return reject(err || new Error("No blogs found"));
         }
-      );
+
+        const blogCount = blogIDs.length;
+        console.log(`Found ${blogCount} blog${blogCount !== 1 ? "s" : ""} to process.`);
+
+        // Ask for confirmation, then process
+        getConfirmation(
+          `Are you sure you want to process all ${blogCount} blog${blogCount !== 1 ? "s" : ""}?`
+        )
+          .then((confirmed) => {
+            if (!confirmed) {
+              console.log("Processing cancelled.");
+              return reject(new Error("Processing cancelled by user"));
+            }
+
+            // Process all blogs
+            // Wrap async function to ensure next() is always called
+            eachBlog(
+              function (_user, blog, next) {
+                // Wrap in promise to handle both sync and async errors
+                Promise.resolve()
+                  .then(() => processBlog(blog))
+                  .then(() => {
+                    next();
+                  })
+                  .catch((err) => {
+                    console.error(`Error processing blog ${blog.id}:`, err.message);
+                    next();
+                  });
+              },
+              (err) => {
+                if (err) return reject(err);
+                resolve();
+              }
+            );
+          })
+          .catch((confirmationErr) => {
+            reject(confirmationErr);
+          });
+      });
     });
   }
 };
