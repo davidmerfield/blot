23b7fe82586145745116153cc4549ed511b55743
David Merfield <dmerfield@gmail.com>
2025-11-18 13:04:08 +0100

    Error handling

--- scripts/tests/util/templates.js
+++ scripts/tests/util/templates.js
@@ -4,7 +4,8 @@ const clfdate = require('helper/clfdate');
 module.exports = function(){
     beforeAll(function(done){
       console.log(clfdate(), '[tests] Building global templates');
-      require('templates')({watch: false}, function(){
+      require('templates')({watch: false}, function(err){
+        if (err) return done(err);
         console.log(clfdate(), '[tests] Built global templates');
         done();
       })
