2ae388b02eaa5446b5706663de269c0e297f6375
David Merfield <dmerfield@gmail.com>
2025-11-24 12:17:21 +0100

    Add tests for entries retrieve loader

---
+++ app/blog/tests/retrieve-entries.js
@@ -0,0 +1,57 @@
+const Entries = require("models/entries");
+const retrieveEntries = require("../render/retrieve/entries");
+
+describe("retrieve entries", function () {
+  it("passes template locals and params to entries.getPage", function (done) {
+    const getPageSpy = spyOn(Entries, "getPage").and.callFake(
+      (blogID, options, callback) => {
+        callback(null, ["first", "second"], { current: 3 });
+      }
+    );
+
+    const req = {
+      blog: { id: "blog-id" },
+      template: { locals: { sort_by: "date", sort_order: "asc", page_size: 10 } },
+      params: { page: 3 },
+      log: function () {},
+    };
+
+    retrieveEntries(req, {}, function (err, locals) {
+      expect(err).toBeFalsy();
+      expect(getPageSpy).toHaveBeenCalledWith(
+        "blog-id",
+        { sortBy: "date", order: "asc", pageNumber: 3, pageSize: 10 },
+        jasmine.any(Function)
+      );
+      expect(locals.entries).toEqual(["first", "second"]);
+      expect(locals.pagination).toEqual({ current: 3 });
+      done();
+    });
+  });
+
+  it("uses query page when route params are missing", function (done) {
+    const getPageSpy = spyOn(Entries, "getPage").and.callFake(
+      (blogID, options, callback) => {
+        callback(null, [], { current: 7 });
+      }
+    );
+
+    const req = {
+      blog: { id: "blog-id" },
+      template: { locals: {} },
+      query: { page: 7 },
+      log: function () {},
+    };
+
+    retrieveEntries(req, {}, function (err, locals) {
+      expect(err).toBeFalsy();
+      expect(getPageSpy).toHaveBeenCalledWith(
+        "blog-id",
+        { sortBy: undefined, order: undefined, pageNumber: 7, pageSize: undefined },
+        jasmine.any(Function)
+      );
+      expect(locals.pagination).toEqual({ current: 7 });
+      done();
+    });
+  });
+});
