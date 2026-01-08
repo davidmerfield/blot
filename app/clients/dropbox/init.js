f5b4665abbd4d23c9354f285a94b6a3a89fc10ac
David Merfield <dmerfield@gmail.com>
2026-01-08 17:30:03 +0100

    Add startup resync and pre-sync timestamp

--- app/clients/dropbox/init.js
+++ app/clients/dropbox/init.js
@@ -4,6 +4,7 @@ const Blog = require("models/blog");
 const clfdate = require("helper/clfdate");
 const email = require("helper/email");
 const resetToBlot = require("./sync/reset-to-blot");
+const sync = require("clients/dropbox/sync");
 const { get: getAccount } = require("./database");
 
 const getAllIDs = promisify(Blog.getAllIDs);
@@ -11,6 +12,7 @@ const getBlog = promisify(Blog.get);
 const getDropboxAccount = promisify(getAccount);
 
 const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const FIFTEEN_MINUTES_IN_MS = 15 * 60 * 1000;
 
 const countChanges = (summary = {}) => {
   return (
@@ -94,7 +96,59 @@ const runValidation = async () => {
   });
 };
 
+const resyncRecentSyncsOnStartup = async () => {
+  console.log(clfdate(), "Dropbox: Checking for recent syncs on startup");
+
+  let blogIDs = [];
+
+  try {
+    blogIDs = await getAllIDs();
+  } catch (err) {
+    console.error(clfdate(), "Dropbox: Failed to load blog IDs", err);
+    return;
+  }
+
+  for (const blogID of blogIDs) {
+    try {
+      const blog = await getBlog({ id: blogID });
+      if (!blog || blog.client !== "dropbox") continue;
+
+      const account = await getDropboxAccount(blogID);
+      if (!account || typeof account.last_sync !== "number") continue;
+
+      if (Date.now() - account.last_sync >= FIFTEEN_MINUTES_IN_MS) continue;
+
+      console.log(clfdate(), "Dropbox: Resyncing recent blog", blogID);
+
+      setImmediate(function () {
+        sync(blog, function (err) {
+          if (err) {
+            console.error(
+              clfdate(),
+              "Dropbox: Resync error for blog",
+              blogID,
+              err
+            );
+          } else {
+            console.log(clfdate(), "Dropbox: Resync complete for blog", blogID);
+          }
+        });
+      });
+    } catch (err) {
+      console.error(
+        clfdate(),
+        "Dropbox: Error checking recent sync for blog",
+        blogID,
+        err
+      );
+    }
+  }
+};
+
 module.exports = async function init() {
   console.log(clfdate(), "Dropbox: Scheduling daily sync validation");
   scheduler.scheduleJob({ hour: 2, minute: 0 }, runValidation);
+  resyncRecentSyncsOnStartup().catch(function (err) {
+    console.error(clfdate(), "Dropbox: Startup resync failed", err);
+  });
 };
