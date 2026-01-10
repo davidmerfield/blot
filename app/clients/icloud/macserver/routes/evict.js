8b14f9a564d9d46aaa67a8019ab29951694139a9
David Merfield <dmerfield@gmail.com>
2026-01-10 16:36:39 +0100

    Path validation

--- app/clients/icloud/macserver/routes/evict.js
+++ app/clients/icloud/macserver/routes/evict.js
@@ -1,4 +1,4 @@
-const { join } = require("path");
+const { join, resolve, isAbsolute, sep } = require("path");
 const { iCloudDriveDirectory } = require("../config");
 
 const brctl = require("../brctl");
@@ -7,17 +7,30 @@ const { unwatch, watch } = require("../watcher");
 
 module.exports = async (req, res) => {
   const blogID = req.header("blogID");
-  const path = Buffer.from(req.header("pathBase64"), "base64").toString("utf8");
+  const pathBase64 = req.header("pathBase64");
+  const path = pathBase64
+    ? Buffer.from(pathBase64, "base64").toString("utf8")
+    : "";
 
   // Validate required headers
-  if (!blogID || !path) {
-    return res.status(400).send("Missing required headers: blogID or path");
+  if (!blogID || !pathBase64 || !path) {
+    return res
+      .status(400)
+      .send("Missing required headers: blogID or path");
+  }
+
+  if (isAbsolute(path)) {
+    return res.status(400).send("Absolute paths are not allowed");
   }
 
   console.log(`Received evict request for blogID: ${blogID}, path: ${path}`);
 
-  const filePath = join(iCloudDriveDirectory, blogID, path);
-  const blogFolder = join(iCloudDriveDirectory, blogID);
+  const basePath = resolve(join(iCloudDriveDirectory, blogID));
+  const filePath = resolve(basePath, path);
+
+  if (filePath !== basePath && !filePath.startsWith(basePath + sep)) {
+    return res.status(400).send("Path escapes blog directory");
+  }
 
   // first unwatch the blogID to prevent further events from being triggered
   await unwatch(blogID);
