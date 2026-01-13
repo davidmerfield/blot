2265d8397170dd282936cebb128c588a618944c8
David Merfield <dmerfield@gmail.com>
2026-01-13 11:13:48 +0100

    Parsing superscript, subscript, underline and strikethrough in google docs

--- app/build/converters/gdoc/index.js
+++ app/build/converters/gdoc/index.js
@@ -116,6 +116,26 @@ async function read(blog, path, callback) {
       $(this).replaceWith("<strong>" + $(this).html() + "</strong>");
     });
 
+    // replace superscript inlines with sup
+    $('span[style*="vertical-align:super"]').each(function (i, elem) {
+      $(this).replaceWith("<sup>" + $(this).html() + "</sup>");
+    });
+
+    // replace subscript inlines with sub
+    $('span[style*="vertical-align:sub"]').each(function (i, elem) {
+      $(this).replaceWith("<sub>" + $(this).html() + "</sub>");
+    });
+
+    // replace underline inlines with u
+    $('span[style*="text-decoration:underline"]').each(function (i, elem) {
+      $(this).replaceWith("<u>" + $(this).html() + "</u>");
+    });
+
+    // replace strikethrough inlines with strike
+    $('span[style*="text-decoration:line-through"]').each(function (i, elem) {
+      $(this).replaceWith("<strike>" + $(this).html() + "</strike>");
+    });
+
     // remove all inline style attributes
     $("[style]").removeAttr("style");
 
