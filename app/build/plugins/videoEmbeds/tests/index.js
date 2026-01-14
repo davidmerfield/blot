2d7127ac36635aaa456ca3052c9aa3c67d7702e2
David Merfield <dmerfield@gmail.com>
2026-01-14 20:39:09 +0100

    test: mock bandcamp embeds (#1316)

--- app/build/plugins/videoEmbeds/tests/index.js
+++ app/build/plugins/videoEmbeds/tests/index.js
@@ -1,8 +1,42 @@
 describe("video-embed plugin", function () {
   const videoEmbed = require("../index");
   const cheerio = require("cheerio");
+  const nock = require("nock");
+  const bandcampOgHtml = ({ src, width, height }) => `<!doctype html>
+    <html>
+      <head>
+        <meta property="og:video" content="${src}">
+        <meta property="og:video:width" content="${width}">
+        <meta property="og:video:height" content="${height}">
+      </head>
+      <body></body>
+    </html>`;
+
+  afterEach(function () {
+    nock.cleanAll();
+  });
 
   it("embeds videos from youtube, vimeo and music from bandcamp", function (done) {
+    nock("https://oliviachaney.bandcamp.com")
+      .get("/album/circus-of-desire")
+      .reply(
+        200,
+        bandcampOgHtml({
+          src: "https://bandcamp.com/EmbeddedPlayer/v=2/album=2447688282/size=large/tracklist=false/artwork=small/",
+          width: 400,
+          height: 120,
+        })
+      );
+    nock("https://cloquet.bandcamp.com")
+      .get("/track/new-drugs")
+      .reply(
+        200,
+        bandcampOgHtml({
+          src: "https://bandcamp.com/EmbeddedPlayer/v=2/track=2483181576/size=large/tracklist=false/artwork=small/",
+          width: 400,
+          height: 120,
+        })
+      );
     const $ = cheerio.load(
       `<a href="https://www.youtube.com/watch?v=MJ62hh0a9U4">https://www.youtube.com/watch?v=MJ62hh0a9U4</a>
         <a href="https://vimeo.com/87952436">https://vimeo.com/87952436</a>
