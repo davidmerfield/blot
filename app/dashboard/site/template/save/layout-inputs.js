fb2c373d50ddefb1229baae475392f654ce453d2
David Merfield <dmerfield@gmail.com>
2026-01-20 16:04:14 +0100

    Add index sort control mapping

--- app/dashboard/site/template/save/layout-inputs.js
+++ app/dashboard/site/template/save/layout-inputs.js
@@ -1,10 +1,35 @@
 
+const SORT_OPTIONS = require("../sort-options");
+
+const DEFAULT_SORT = {
+  sort_by: "date",
+  sort_order: "desc"
+};
+
+const resolveSortSelection = value => {
+  const matched = SORT_OPTIONS.find(option => option.value === value);
+  return matched || SORT_OPTIONS.find(
+    option =>
+      option.sort_by === DEFAULT_SORT.sort_by &&
+      option.sort_order === DEFAULT_SORT.sort_order
+  );
+};
+
 module.exports = function (req, res, next) {
   // the user has not clicked on a button in the 'color scheme' list
   if (req.locals.thumbnails_per_row && req.locals.number_of_rows) {
     req.locals.page_size =
       parseInt(req.locals.thumbnails_per_row) *
       parseInt(req.locals.number_of_rows);
+  }
+
+  const sortSelection = resolveSortSelection(req.locals.sort_by);
+  if (sortSelection) {
+    req.locals.sort_by = sortSelection.sort_by;
+    req.locals.sort_order = sortSelection.sort_order;
+  } else {
+    req.locals.sort_by = DEFAULT_SORT.sort_by;
+    req.locals.sort_order = DEFAULT_SORT.sort_order;
   }
 
   next();
---
+++ app/dashboard/site/template/sort-options.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    label: "Publish date - Newest first",
+    sort_by: "date",
+    sort_order: "desc",
+    value: "date_desc"
+  },
+  {
+    label: "Publish date - Oldest first",
+    sort_by: "date",
+    sort_order: "asc",
+    value: "date_asc"
+  },
+  {
+    label: "File path - A to Z",
+    sort_by: "id",
+    sort_order: "desc",
+    value: "id_desc"
+  },
+  {
+    label: "File path - Z to A",
+    sort_by: "id",
+    sort_order: "asc",
+    value: "id_asc"
+  }
+];
