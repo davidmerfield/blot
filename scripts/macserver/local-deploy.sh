31b09db26a0b8ac58b1e0d2e0457c7060b4783a0
David Merfield <dmerfield@gmail.com>
2026-01-21 10:41:40 +0100

    Update local deploy script

--- scripts/macserver/local-deploy.sh
+++ scripts/macserver/local-deploy.sh
@@ -1,3 +1,3 @@
 scp -r ./app/clients localmacserver:~/blot/app/
 
-ssh localmacserver "export NVM_DIR=\"\$HOME/.nvm\" && [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" && pm2 restart macserver"
+ssh localmacserver "export NVM_DIR=\"\$HOME/.nvm\" && [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" && cd ~/blot/app/clients/icloud/macserver && rm package-lock.json && rm -rf node_modules && npm install && pm2 restart macserver"
