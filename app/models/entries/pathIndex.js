99677fdcb066ae37291c6ba55f71c5bcac6d121b
David Merfield <dmerfield@gmail.com>
2026-02-13 18:23:55 +0100

    Remove entries path index lazy backfill fallback

--- app/models/entries/pathIndex.js
+++ app/models/entries/pathIndex.js
@@ -1,10 +1,6 @@
 var ensure = require("helper/ensure");
 var redis = require("models/client");
 
-var LOCK_TTL_SECONDS = 30;
-var WAIT_RETRIES = 8;
-var WAIT_DELAY_MS = 50;
-
 function entriesKey(blogID) {
   return "blog:" + blogID + ":entries";
 }
@@ -15,22 +11,6 @@ function lexKey(blogID) {
 
 function readyKey(blogID) {
   return "blog:" + blogID + ":entries:lex:ready";
-}
-
-function lockKey(blogID) {
-  return "blog:" + blogID + ":entries:lex:backfill:lock";
-}
-
-function waitForReady(blogID, retries, callback) {
-  if (retries <= 0) return callback();
-
-  setTimeout(function () {
-    redis.exists(readyKey(blogID), function (err, ready) {
-      if (err) return callback(err);
-      if (ready) return callback();
-      waitForReady(blogID, retries - 1, callback);
-    });
-  }, WAIT_DELAY_MS);
 }
 
 function backfillIndex(blogID, callback) {
@@ -48,7 +28,6 @@ function backfillIndex(blogID, callback) {
     }
 
     multi.set(readyKey(blogID), "1");
-    multi.del(lockKey(blogID));
 
     multi.exec(function (err) {
       if (err) return callback(err);
@@ -63,29 +42,7 @@ function ensureIndex(blogID, callback) {
   redis.exists(readyKey(blogID), function (err, ready) {
     if (err) return callback(err);
     if (ready) return callback();
-
-    // TEMPORARY ROLLOUT FALLBACK:
-    // This lazy backfill path is here to support seamless production rollout.
-    // After running scripts/blog/backfill-entries-path-index.js for all blogs in
-    // production, this block (and its lock/wait helpers) can be removed.
-    redis.set(
-      lockKey(blogID),
-      "1",
-      "NX",
-      "EX",
-      LOCK_TTL_SECONDS,
-      function (err, locked) {
-        if (err) return callback(err);
-
-        if (!locked) {
-          return waitForReady(blogID, WAIT_RETRIES, callback);
-        }
-
-        backfillIndex(blogID, function (err) {
-          callback(err);
-        });
-      }
-    );
+    callback(new Error("Entries path index is not ready for blog " + blogID));
   });
 }
 
