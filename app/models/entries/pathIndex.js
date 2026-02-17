8d6cf9b10dd5fd4d93f6d1b0b4aa7ff782a787b8
David Merfield <dmerfield@gmail.com>
2026-02-17 11:17:20 +0100

    Restore backfill check

--- app/models/entries/pathIndex.js
+++ app/models/entries/pathIndex.js
@@ -1,3 +1,10 @@
+var ensure = require("helper/ensure");
+var redis = require("models/client");
+
+function entriesKey(blogID) {
+  return "blog:" + blogID + ":entries";
+}
+
 function lexKey(blogID) {
   return "blog:" + blogID + ":entries:lex";
 }
@@ -6,7 +13,33 @@ function readyKey(blogID) {
   return "blog:" + blogID + ":entries:lex:ready";
 }
 
+function backfillIndex(blogID, callback) {
+  ensure(blogID, "string").and(callback, "function");
+
+  redis.zrange(entriesKey(blogID), 0, -1, function (err, ids) {
+    if (err) return callback(err);
+
+    var multi = redis.multi();
+
+    multi.del(lexKey(blogID));
+
+    if (ids && ids.length) {
+      for (var i = 0; i < ids.length; i++) {
+        multi.zadd(lexKey(blogID), 0, ids[i]);
+      }
+    }
+
+    multi.set(readyKey(blogID), "1");
+
+    multi.exec(function (err) {
+      if (err) return callback(err);
+      callback(null, ids ? ids.length : 0);
+    });
+  });
+}
+
 module.exports = {
   lexKey: lexKey,
   readyKey: readyKey,
+  backfillIndex: backfillIndex,
 };
