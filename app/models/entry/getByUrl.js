6abcc925ab27f210ad6bae750a64178aa191a508
David Merfield <dmerfield@gmail.com>
2026-02-23 15:01:00 +0100

    Revert "x"
    This reverts commit 53e27a73f9c694e0caf8ac3e1d3a4e6927408487.

--- app/models/entry/getByUrl.js
+++ app/models/entry/getByUrl.js
@@ -1,5 +1,4 @@
 var ensure = require("helper/ensure");
-var urlNormalizer = require("helper/urlNormalizer");
 var redis = require("models/client");
 var get = require("./get");
 var urlKey = require("./key").url;
@@ -14,10 +13,6 @@ module.exports = function getByUrl(blogID, entryUrl, callback) {
   } catch (e) {
     // leave as-is if decoding fails (malformed %)
   }
-
-  // Normalize to canonical pathname before lookup so internal link variants
-  // with query strings and fragments resolve to the same entry URL key.
-  entryUrl = urlNormalizer(entryUrl);
 
   redis.get(urlKey(blogID, entryUrl), function (error, entryID) {
     if (error) throw error;
