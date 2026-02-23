5d3e9a24aa5bd5bf87c0808c421428627dbf9cb6
David Merfield <dmerfield@gmail.com>
2026-02-23 14:03:17 +0100

    Fix

--- app/models/entry/getByUrl.js
+++ app/models/entry/getByUrl.js
@@ -6,6 +6,14 @@ var urlKey = require("./key").url;
 module.exports = function getByUrl(blogID, entryUrl, callback) {
   ensure(blogID, "string").and(entryUrl, "string").and(callback, "function");
 
+  // Normalize to decoded form so percent-encoded hrefs (e.g. from Pandoc)
+  // resolve to entries stored under decoded URLs (see _setUrl.js).
+  try {
+    entryUrl = decodeURI(entryUrl);
+  } catch (e) {
+    // leave as-is if decoding fails (malformed %)
+  }
+
   redis.get(urlKey(blogID, entryUrl), function (error, entryID) {
     if (error) throw error;
 
