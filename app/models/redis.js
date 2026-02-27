62631fec0df79d49c4c60bbe15429be10708b2c2
David Merfield <dmerfield@gmail.com>
2026-02-27 14:43:08 +0100

    Remove redis client.batch compatibility alias (#1498)

--- app/models/redis.js
+++ app/models/redis.js
@@ -468,10 +468,6 @@ module.exports = function () {
     };
   }
 
-  if (typeof client.batch !== "function") {
-    client.batch = client.multi.bind(client);
-  }
-
   if (typeof client.quit === "function") {
     const nativeQuit = client.quit.bind(client);
     client.quit = function compatQuit(callback) {
