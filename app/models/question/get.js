825630bbb9e8e654dccb2538cfb01cc4facf7706
David Merfield <dmerfield@gmail.com>
2026-02-27 17:20:02 +0100

    Migrate question model to client-new redis API

--- app/models/question/get.js
+++ app/models/question/get.js
@@ -1,54 +1,50 @@
-const client = require("models/client");
+const client = require("models/client-new");
 const keys = require("./keys");
 const moment = require("moment");
 
-module.exports = id => {
-  return new Promise((resolve, reject) => {
-    (async () => {
-      const [reply_ids, question, last_reply_created_at] = await Promise.all([
-        client.zrange(keys.children(id), 0, -1),
-        client.hgetall(keys.item(id)),
-        client.zscore(keys.by_last_reply, id),
-      ]);
+module.exports = async (id) => {
+  const [reply_ids, question, last_reply_created_at] = await Promise.all([
+    client.zRange(keys.children(id), 0, -1),
+    client.hGetAll(keys.item(id)),
+    client.zScore(keys.by_last_reply, id),
+  ]);
 
-      if (!question) return resolve(null);
+  if (!question || !Object.keys(question).length) return null;
 
-      const replies = await Promise.all(
-        reply_ids.map((reply_id) => {
-          return client.hgetall(keys.item(reply_id));
-        })
-      );
+  const replies = await Promise.all(
+    reply_ids.map((reply_id) => {
+      return client.hGetAll(keys.item(reply_id));
+    })
+  );
 
-      try {
-        question.tags = JSON.parse(question.tags);
-      } catch (e) {
-        question.tags = [];
-      }
+  try {
+    question.tags = JSON.parse(question.tags);
+  } catch (e) {
+    question.tags = [];
+  }
 
-      question.replies = replies.map((reply) => {
-        const date = new Date(parseInt(reply.created_at, 10));
-        reply.time = moment(date).fromNow();
-        return reply;
-      });
-
-      const createdDate = new Date(parseInt(question.created_at, 10));
-      const createdTime = moment(createdDate).fromNow();
-
-      const hasLastReplyTimestamp =
-        last_reply_created_at !== null &&
-        !Number.isNaN(parseInt(last_reply_created_at, 10));
-      const lastReplyTimestamp = hasLastReplyTimestamp
-        ? last_reply_created_at
-        : question.created_at;
-      const lastReplyDate = new Date(parseInt(lastReplyTimestamp, 10));
-
-      question.number_of_replies = replies.length;
-      question.last_reply_created_at = lastReplyTimestamp;
-      question.last_reply_time = moment(lastReplyDate).fromNow();
-      question.created_time = createdTime;
-      question.time = createdTime;
-
-      resolve(question);
-    })().catch(reject);
+  question.replies = replies.map((reply) => {
+    const date = new Date(parseInt(reply.created_at, 10));
+    reply.time = moment(date).fromNow();
+    return reply;
   });
+
+  const createdDate = new Date(parseInt(question.created_at, 10));
+  const createdTime = moment(createdDate).fromNow();
+
+  const hasLastReplyTimestamp =
+    last_reply_created_at !== null &&
+    !Number.isNaN(parseInt(last_reply_created_at, 10));
+  const lastReplyTimestamp = hasLastReplyTimestamp
+    ? last_reply_created_at
+    : question.created_at;
+  const lastReplyDate = new Date(parseInt(lastReplyTimestamp, 10));
+
+  question.number_of_replies = replies.length;
+  question.last_reply_created_at = lastReplyTimestamp;
+  question.last_reply_time = moment(lastReplyDate).fromNow();
+  question.created_time = createdTime;
+  question.time = createdTime;
+
+  return question;
 };
--- app/models/question/tests/setup.js
+++ app/models/question/tests/setup.js
@@ -1,35 +1,21 @@
 module.exports = function setup(options) {
   // remove all keys from the redis-db with the prefix 'blot:questions
-  beforeEach(function (done) {
-    const client = require("models/client");
+  beforeEach(async function () {
+    const client = require("models/client-new");
 
-    client.keys("blot:questions:*", function (err, keys) {
-      if (err) return done(err);
-      if (keys.length > 0) {
-        client.del(keys, function (err, response) {
-          if (err) return done(err);
-          done();
-        });
-      } else {
-        done();
-      }
-    });
+    const keys = await client.keys("blot:questions:*");
+    if (keys.length > 0) {
+      await client.del(keys);
+    }
   });
 
   // remove all keys from the redis-db with the prefix 'blot:questions
-  afterEach(function (done) {
-    const client = require("models/client");
+  afterEach(async function () {
+    const client = require("models/client-new");
 
-    client.keys("blot:questions:*", function (err, keys) {
-      if (err) return done(err);
-      if (keys.length > 0) {
-        client.del(keys, function (err, response) {
-          if (err) return done(err);
-          done();
-        });
-      } else {
-        done();
-      }
-    });
+    const keys = await client.keys("blot:questions:*");
+    if (keys.length > 0) {
+      await client.del(keys);
+    }
   });
 };
