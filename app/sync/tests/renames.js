4c01ff2941b88920631312ea1e89c9bc4066a257
David Merfield <dmerfield@gmail.com>
2026-02-28 11:10:08 +0100

    Migrate test Redis imports and async client-new usage

--- app/sync/tests/renames.js
+++ app/sync/tests/renames.js
@@ -2,7 +2,7 @@ describe("update", function () {
   var sync = require("../index");
   var fs = require("fs-extra");
   var async = require("async");
-  var redis = require("models/client");
+  var redis = require("models/client-new");
   var entryKey = require("models/entry/key").entry;
 
   it("detects a renamed file across multiple syncs", function (done) {
@@ -209,18 +209,18 @@ describe("update", function () {
                     });
                   },
                   function (next) {
-                    redis.del(entryKey(blogID, ghostPath), function (err) {
-                      if (err) return next(err);
+                    redis
+                      .del(entryKey(blogID, ghostPath))
+                      .then(function () {
+                        var deletedListKey = "blog:" + blogID + ":deleted";
 
-                      var deletedListKey = "blog:" + blogID + ":deleted";
-
-                      redis.zscore(deletedListKey, ghostEntryID, function (err, score) {
-                        if (err) return next(err);
-
+                        return redis.zScore(deletedListKey, ghostEntryID);
+                      })
+                      .then(function (score) {
                         expect(score).not.toBeNull();
                         next();
-                      });
-                    });
+                      })
+                      .catch(next);
                   },
                 ],
                 function (err) {
