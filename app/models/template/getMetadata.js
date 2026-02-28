a66e19b98b9c97fd4630e1603747051d5f926908
David Merfield <dmerfield@gmail.com>
2026-02-28 10:46:10 +0100

    x

--- app/models/template/getMetadata.js
+++ app/models/template/getMetadata.js
@@ -5,11 +5,20 @@ var metadataModel = require("./metadataModel");
 
 module.exports = function getMetadata(id, callback) {
   client.hGetAll(key.metadata(id)).then(function (metadata) {
+    var rawHasFields = metadata && Object.keys(metadata).length > 0;
+
+    if (!rawHasFields) {
+      const err = new Error("No template: " + id);
+      err.code = "ENOENT";
+      return callback(err, null);
+    }
 
     metadata = deserialize(metadata, metadataModel);
 
-    if (!metadata) {
-      err = new Error("No template: " + id);
+    var metadataHasFields = metadata && Object.keys(metadata).length > 0;
+
+    if (!metadataHasFields) {
+      const err = new Error("No template: " + id);
       err.code = "ENOENT";
       return callback(err, null);
     }
